291975321d1b30dd46a581e8aec9ed5324d47e81
jian <jian_kowalski@hotmail.com>
2022-06-29 12:56:30 -0300

    ci(Alterando a pipeline):
    Alterando a pipeline

--- src/main/java/com/jiankowalski/basicspring/datasources/config/DatasourceConfiguration.java
+++ src/main/java/com/jiankowalski/basicspring/datasources/config/DatasourceConfiguration.java
@@ -3,14 +3,12 @@ package com.jiankowalski.basicspring.datasources.config;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories("com.jiankowalski.basicspring.datasources")
-@Profile("PRD")
 public class DatasourceConfiguration {
 
   @Bean
